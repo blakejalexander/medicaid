a1cb2fae0103f08356492ec15be551b49cc930f8
blakejalexander <blake.alexander@uqconnect.edu.au>
2017-06-08 00:42:20 +1000

    added neural network saving to training.py
    TensorFlow is really poorly documented but I managed to get model exportation
    working by trawling around on GitHub. The next step is to import it using
    TensorFlow in Android...

--- machine_learning/training.py
+++ machine_learning/training.py
@@ -9,11 +9,19 @@ import sys
 
 import numpy as np
 
-# Import tensorflow, reduce log level so we don't see warnings about the
+# Reduce tensorflow log level so we don't see warnings about the
 # distribution of TensorFlow not supporting instructions available on this
 # machine etc. etc.
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
 import tensorflow as tf
+from tensorflow.contrib.learn.python.learn.utils import input_fn_utils
+from tensorflow.python.ops import array_ops
+from tensorflow.python.framework import dtypes
+from tensorflow.contrib.learn import build_parsing_serving_input_fn
+from tensorflow.contrib.layers import create_feature_spec_for_parsing
+
+tf.logging.set_verbosity(tf.logging.INFO)
 
 # Data sets
 MEDICAID_TRAINING = "training.csv"
@@ -21,9 +29,13 @@ MEDICAID_TEST = "test.csv"
 
 # Model directory
 MEDICAID_MODEL_DIR = os.getcwd() + "/medicaid_model"
+MEDICAID_MODEL_DIR_TRAINED = MEDICAID_MODEL_DIR + "/trained"
 
-# Number of classes to classify as
+# Number of classes to classify to
 NUMBER_CLASSES = 4
+
+# Number of features to classify from
+NUMBER_FEATURES = 4
 
 def class_to_label(label):
     if label == 0:
@@ -59,13 +71,15 @@ def main():
             features_dtype=np.float32)
 
     # Specify that all features have real-value data
-    feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
+    feature_columns = [tf.contrib.layers.real_valued_column("",
+        dimension=NUMBER_FEATURES)]
 
     # Build 3 layer DNN (classifier) with 10, 20, 10 hidden units respectively.
     classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
-                                                hidden_units=[ 10, 30, 10],
-                                                n_classes=NUMBER_CLASSES,
-                                                model_dir=MEDICAID_MODEL_DIR)
+        hidden_units=[ 10, 20, 10],
+        n_classes=NUMBER_CLASSES,
+        model_dir=MEDICAID_MODEL_DIR,
+        config=tf.contrib.learn.RunConfig(save_checkpoints_secs=1))
 
     # Define the training inputs
     def get_train_inputs():
@@ -88,20 +102,38 @@ def main():
     accuracy_score = classifier.evaluate(input_fn=get_test_inputs,
         steps=1)["accuracy"]
 
-    print(("\nTest Accuracy: {0:f}\n".format(accuracy_score)))
+    print(("\nAccuracy: {0:f}\n".format(accuracy_score)))
 
-    # Classify new samples, one of each event type, a jump and a fall
+    # Classify new samples, one of each event type, a walk and a fall
     # TODO: Blake - these samples are just rounded values of some random rows
     # in the test set.
     def new_samples():
         return np.array(
-            [[ 505,	0.82,	16.38,	9.91 ],
+            [[ 1077,0.49, 10.33,10.52 ],
              [ 346,	0.95,	19.37,	9.89 ]], dtype=np.float32)
 
-    predictions = list(classifier.predict(input_fn=new_samples))
+    predictions = list(classifier.predict_classes(input_fn=new_samples))
     predictions = [ class_to_label(i) for i in predictions ]
 
-    print(("New Samples, Class Predictions:    {}\n".format(predictions)))
+    # Classify some samples, just to get a feel for the model success.
+    print("==== Sample Classifications ====")
+    print("Prediction: \t{}".format(predictions))
+    print("Answer: \t{}\n".format( [class_to_label(2), class_to_label(0)] ))
+
+
+    # Save the model to a binary .pb file for loading into Java (on Android)
+    # See: https://github.com/tensorflow/tensorflow/issues/3340
+    # TensorFlow is REALLY poorly documented. This seems to work. I don't
+    # think anyone actually knows why.
+    # Actually this dude got somewhere:
+    # https://github.com/MtDersvan/tf_playground/blob/master/wide_and_deep_tutorial/wide_and_deep_basic_serving.md
+    tfrecord_serving_input_fn = build_parsing_serving_input_fn(
+        create_feature_spec_for_parsing(feature_columns))
+    classifier.export_savedmodel(
+        export_dir_base=MEDICAID_MODEL_DIR_TRAINED,
+        serving_input_fn = tfrecord_serving_input_fn,
+        as_text=False)
+
 
 if __name__ == "__main__":
-        main()
+    main()
