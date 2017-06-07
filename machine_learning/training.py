dd4c68a9442fbad701f0bbf7accf2fa83895e80e
blakejalexander <blake.alexander@uqconnect.edu.au>
2017-06-07 15:39:05 +1000

    Added initial offline training file

---
+++ machine_learning/training.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+""" Medicaid neural network classification model training file.
+    Modified from the TensorFlow Iris flower classification example.
+    See: https://www.tensorflow.org/get_started/tflearn
+"""
+
+import os
+import sys
+
+import numpy as np
+
+# Import tensorflow, reduce log level so we don't see warnings about the
+# distribution of TensorFlow not supporting instructions available on this
+# machine etc. etc.
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+import tensorflow as tf
+
+# Data sets
+MEDICAID_TRAINING = "training.csv"
+MEDICAID_TEST = "test.csv"
+
+# Model directory
+MEDICAID_MODEL_DIR = os.getcwd() + "/medicaid_model"
+
+def main():
+
+    # If the training and test sets aren't stored locally, abort mission
+    if not os.path.exists(MEDICAID_TRAINING):
+        print("error: no training data")
+        sys.exit(1)
+
+    if not os.path.exists(MEDICAID_TEST):
+        print("error: no test data")
+        sys.exit(1)
+
+    # Load datasets.
+    training_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+            filename=MEDICAID_TRAINING,
+            target_dtype=np.int,
+            features_dtype=np.float32)
+    test_set = tf.contrib.learn.datasets.base.load_csv_with_header(
+            filename=MEDICAID_TEST,
+            target_dtype=np.int,
+            features_dtype=np.float32)
+
+    # Specify that all features have real-value data
+    feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
+
+    # Build 3 layer DNN (classifier) with 10, 20, 10 hidden units respectively.
+    classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
+                                                hidden_units=[10, 20, 10],
+                                                n_classes=3,
+                                                model_dir=MEDICAID_MODEL_DIR)
+
+    # Define the training inputs
+    def get_train_inputs():
+        x = tf.constant(training_set.data)
+        y = tf.constant(training_set.target)
+
+        return x, y
+
+    # Fit model.
+    classifier.fit(input_fn=get_train_inputs, steps=200000)
+
+    # Define the test inputs
+    def get_test_inputs():
+        x = tf.constant(test_set.data)
+        y = tf.constant(test_set.target)
+
+        return x, y
+
+    # Evaluate the accuracy of the model after training accuracy.
+    accuracy_score = classifier.evaluate(input_fn=get_test_inputs,
+        steps=1)["accuracy"]
+
+    print(("\nTest Accuracy: {0:f}\n".format(accuracy_score)))
+
+    # Classify new samples, one of each event type, a jump and a fall
+    # TODO: Blake - these samples are just rounded values of some random rows
+    # in the test set.
+    def new_samples():
+        return np.array(
+            [[ 505,	0.82,	16.38,	9.91 ],
+             [ 346,	0.95,	19.37,	9.89 ]], dtype=np.float32)
+
+    def class_to_label(label):
+        if label == 0:
+            return "Fall"
+        elif label == 1:
+            return "Jump"
+        elif label == 2:
+            return "Walking"
+
+    predictions = list(classifier.predict(input_fn=new_samples))
+    predictions = [ class_to_label(i) for i in predictions ]
+
+    print(("New Samples, Class Predictions:    {}\n".format(predictions)))
+
+if __name__ == "__main__":
+        main()
